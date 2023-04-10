62c717b52c5ec9d2ac0b69ba1c0db2040138b009
jackytam2020 <jackytam2020@gmail.com>
2023-04-10 13:15:49 -0700

    changed db config file

--- confiq/db.js
+++ confiq/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5050;
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
+    app.listen(PORT, () => {
+      console.log(`running at http://localhost:${PORT}`);
+    });
   } catch (error) {
     console.log(error);
     process.exit(1);
--- index.js
+++ index.js
@@ -2,13 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 5050;
+
 const connectDB = require('./confiq/db');
 
 const imagesRoutes = require('./routes/imagesRoute');
 const faqRoutes = require('./routes/faqRoute');
-
-connectDB();
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -22,6 +20,4 @@ app.use(
 app.use('/images', imagesRoutes);
 app.use('/faq', faqRoutes);
 
-app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
-});
+connectDB();
